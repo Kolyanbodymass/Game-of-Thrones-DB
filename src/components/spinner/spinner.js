39f68708734280277badd850df02cbc28568f6e4
Николай Васильев <60035927+Kolyanbodymass@users.noreply.github.com>
2021-02-11 18:41:40 +0500

    React patterns

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-spin-ueeauidw9bp">
+            <div className="ldio-bt0gdk9jjc">
+            <div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div></div>
+        </div>
+    )
+}
+
+export default Spinner;
+
+
